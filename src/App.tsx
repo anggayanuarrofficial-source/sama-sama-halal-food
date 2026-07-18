9694d9b77e0bc1042950e93888dfd1714d908ce3
anggayanuarrofficial-source <anggayanuarrofficial@gmail.com>
2026-07-19 05:33:13 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router'
 import Home from './pages/Home'
+import MenuPage from './pages/Menu'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/menu" element={<MenuPage />} />
     </Routes>
   )
 }
